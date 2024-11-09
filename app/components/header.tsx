29a095808876e76b70cc66992e53cfdbd6516167
skajosborn <saraosborn29@yahoo.com>
2024-11-09 18:23:41 -0500

    making header bigger

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -7,12 +7,12 @@ import Image from 'next/image';
 function Header() {
   return (
     <>
-      <nav className="fixed top-0 w-[125%] h-[60px] sm:h-[70px] md:h-[80px] lg:h-[100px] bg-gray-300 z-50 flex items-center shadow-md px-4 sm:px-5 md:px-6 lg:px-8 transform scale-[0.8] origin-top-left">
+      <nav className="fixed top-0 w-[125%] h-[70px] sm:h-[80px] md:h-[90px] lg:h-[120px] bg-gray-300 z-50 flex items-center shadow-md px-4 sm:px-5 md:px-6 lg:px-8 transform scale-[0.8] origin-top-left">
         {/* Brand/Logo Section */}
-        <div className="text-black ml-10 text-2xl mr-10 sm:text-2xl md:text-3xl lg:text-4xl font-bold font-rouge-script"></div>
+        <div className="text-black ml-10 text-2xl mr-10 sm:text-3xl md:text-4xl lg:text-5xl font-bold font-rouge-script"></div>
         
         {/* Profile Image */}
-        <div className="relative h-[50px] w-[25px] sm:h-[60px] sm:w-[280px] md:h-[70px] md:w-[310px] lg:h-[90px] lg:w-[350px]">
+        <div className="relative h-[60px] w-[30px] sm:h-[70px] sm:w-[300px] md:h-[80px] md:w-[330px] lg:h-[100px] lg:w-[370px]">
           <Image
             src="/images/black4.png"
             alt="Descriptive alt text"
@@ -28,8 +28,8 @@ function Header() {
           {['Home', 'About', 'Projects', 'Blog', 'Contact'].map((label) => (
             <a
               key={label}
-              href={`/#${label.toLowerCase()}`} // Updated to scroll to section IDs
-              className="bg-gray-200 h-[30px] w-[80px] sm:h-[40px] sm:w-[90px] md:h-[50px] md:w-[100px] lg:h-[60px] lg:w-[120px] text-md sm:text-lg md:text-xl lg:text-2xl font-mulish transition-all duration-300 text-black flex items-center justify-center transform hover:scale-105 rounded-md"
+              href={`/#${label.toLowerCase()}`}
+              className="bg-gray-200 h-[35px] w-[90px] sm:h-[45px] sm:w-[100px] md:h-[55px] md:w-[110px] lg:h-[70px] lg:w-[140px] text-md sm:text-lg md:text-xl lg:text-2xl font-mulish transition-all duration-300 text-black flex items-center justify-center transform hover:scale-105 rounded-md"
               style={{
                 transition: 'all 0.3s ease',
                 boxShadow: '3px 3px 5px rgba(0, 0, 0, 0.3), inset -2px -2px 5px rgba(0, 0, 0, 0.2), inset 2px 2px 5px rgba(255, 255, 255, 0.5)',
@@ -59,13 +59,13 @@ function Header() {
 
         {/* Mobile Menu Button */}
         <button className="sm:hidden ml-auto mr-4 text-white relative transform transition-transform active:scale-95 active:translate-y-1">
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 sm:h-8 sm:w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 sm:h-10 sm:w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
           </svg>
         </button>
       </nav>
       {/* Spacer div to push content below fixed header */}
-      <div className="h-[75px] sm:h-[87.5px] md:h-[100px] lg:h-[125px]"></div>
+      <div className="h-[87.5px] sm:h-[100px] md:h-[112.5px] lg:h-[150px]"></div>
     </>
   );
 }
